6bbc67895d027222024d4cf461276a30a1955e0c
Hubert2883 <hubert.dab1245@gmail.com>
2025-04-22 23:32:37 +0200

    rewardmodel_train_new model working for reqard training

--- parquetreader.py
+++ parquetreader.py
@@ -1,10 +1,12 @@
+import pandas as pd
+
+# show every column when printing a DataFrame
+pd.set_option('display.max_columns', None)
+# optionally also expand the width so lines won’t wrap
+pd.set_option('display.width', None)
+
+# now load and show
 import pyarrow.parquet as pq
-
-
-
-
-# Read the Parquet file into a PyArrow Table
-table = pq.read_table("/ceph/home/student.aau.dk/wb68dm/Isaac-GR00T_RL/New_results_0002action/data/chunk-000/episode_000002.parquet")
-# Convert the table to a pandas DataFrame if needed
+table = pq.read_table("/ceph/home/student.aau.dk/wb68dm/Isaac-GR00T_RL/Data_results_0001/Data/chunk-000/episode_000001.parquet")
 df = table.to_pandas()
 print(df)
