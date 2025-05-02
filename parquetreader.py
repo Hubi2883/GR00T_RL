8d327862c5b201390f4fe56640110ebf2ae3bf7a
Hubert2883 <hubert.dab1245@gmail.com>
2025-05-02 12:08:15 +0200

    check

--- parquetreader.py
+++ parquetreader.py
@@ -7,6 +7,6 @@ pd.set_option('display.width', None)
 
 # now load and show
 import pyarrow.parquet as pq
-table = pq.read_table("/ceph/home/student.aau.dk/wb68dm/Isaac-GR00T_RL/Data_results_0001/Data/chunk-000/episode_000001.parquet")
+table = pq.read_table("/ceph/home/student.aau.dk/wb68dm/Isaac-GR00T_RL/Data_results_0001/data/chunk-000/episode_000001.parquet")
 df = table.to_pandas()
 print(df)
